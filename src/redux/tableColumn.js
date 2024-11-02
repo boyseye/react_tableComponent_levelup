bb2a291990cad77e536460c764c73b8ac15f69f7
boyseye <boyseye29@gmail.com>
2024-11-02 11:39:20 +0900

    feat:테이블컴포넌트 공통화

--- src/redux/tableColumn.js
+++ src/redux/tableColumn.js
@@ -1,64 +1,101 @@
 // src/redux/tableColumns.js
 
-export const Col1Enum = Object.freeze({
-  A: 'a',
-  B: 'b',
-  C: 'c',
-});
+export const TableA = {
+  COLUMN_1: Object.freeze({
+    A: 'a',
+    B: 'b',
+    C: 'c',
+  }),
+  
+  COLUMN_2: Object.freeze({
+    D: 'd',
+    E: 'e',
+    F: 'f',
+    G: 'g',
+  }),
 
-export const Col2Enum = Object.freeze({
-  D: 'd',
-  E: 'e',
-  F: 'f',
-  G: 'g',
-});
+  COLUMN_STYLE: [
+    { name: 'a', rowspan: 2, colspan: 1 },
+    { name: 'b', rowspan: 1, colspan: 2 },
+    { name: 'c', rowspan: 1, colspan: 2 },
+  ],
 
-export const AlternativeCol1Enum = Object.freeze({
-  X: 'x',
-  Y: 'y',
-  Z: 'z',
-});
-
-export const AlternativeCol2Enum = Object.freeze({
-  P: 'p',
-  Q: 'q',
-  R: 'r',
-  M: 'm',
-  N: 'n',
-  S: 's',
-});
-
-// Configuration for col1 headers, including rowspan and colspan for each column
-export const Col1Config = [
-  { name: Col1Enum.A, rowspan: 2, colspan: 1 },
-  { name: Col1Enum.B, rowspan: 1, colspan: 2 },
-  { name: Col1Enum.C, rowspan: 1, colspan: 2 },
-];
+  API: 'CALL GET: API "AAAAA"',
+};
 
 
-export const col1api = "api";
-export const col2api = "apib";
+
+export const TableB = {
+  COLUMN_1: Object.freeze({
+    X: 'x',
+    Y: 'y',
+    Z: 'z',
+  }),
+  
+  COLUMN_2: Object.freeze({
+    P: 'p',
+    Q: 'q',
+    R: 'r',
+    M: 'm',
+    N: 'n',
+    S: 's',
+  }),
+
+  COLUMN_STYLE: [
+    { name: 'a', rowspan: 1, colspan: 2 },
+    { name: 'b', rowspan: 1, colspan: 2 },
+    { name: 'c', rowspan: 1, colspan: 2 },
+  ],
+  
+  API: 'CALL GET: API "BBBBB"',
+};
   
 
+export const TableC = {
+  COLUMN_1: Object.freeze({
+    X: 'x1',
+    Y: 'y2',
+    Z: 'z3',
+  }),
+  
+  COLUMN_2: Object.freeze({
+    P: 'p',
+    Q: 'q',
+    R: 'r',
+    M: 'm',
+    N: 'n',
+    S: 's',
+  }),
 
-export const Col2Config = [
-  { name: Col1Enum.A, rowspan: 1, colspan: 2 },
-  { name: Col1Enum.B, rowspan: 1, colspan: 2 },
-  { name: Col1Enum.C, rowspan: 1, colspan: 2 },
-];
+  COLUMN_STYLE: [
+    { name: 'a', rowspan: 1, colspan: 2 },
+    { name: 'b', rowspan: 1, colspan: 2 },
+    { name: 'c', rowspan: 1, colspan: 2 },
+  ],
+  
+  API: 'CALL GET: API "cc"',
+};
+
+
 
 // Define table configurations for different table types
 export const TableConfigurations = {
   tableA: {
-    col1: Object.values(Col1Enum),
-    col2: Object.values(Col2Enum),
-    colConfig: Col1Config,
-    api : col1api
+    col1: Object.values(TableA.COLUMN_1),
+    col2: Object.values(TableA.COLUMN_2),
+    colConfig: TableA.COLUMN_STYLE,
+    api : TableA.API
   },
   tableB: {
-    col1: Object.values(AlternativeCol1Enum),
-    col2: Object.values(AlternativeCol2Enum),
-    colConfig:Col2Config,
-    api : col2api
+    col1: Object.values(TableB.COLUMN_1),
+    col2: Object.values(TableB.COLUMN_2),
+    colConfig:TableB.COLUMN_STYLE,
+    api : TableB.API
+  },
+  tableC: {
+    col1: Object.values(TableC.COLUMN_1),
+    col2: Object.values(TableC.COLUMN_2),
+    colConfig:TableC.COLUMN_STYLE,
+    api : TableC.API
   },
 };
