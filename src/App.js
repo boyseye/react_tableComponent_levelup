b7aa87d7a643c77d30ea67e74f89798000a54a2c
boyseye <boyseye29@gmail.com>
2024-11-02 11:06:30 +0900

    feat:table 컴포넌트 공통화

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Provider } from 'react-redux';
+import TableMain from './components/TableMain';
+import store from './redux/store'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => (
+  <Provider store={store}>
+    {/* Pass 'default' or 'alternative' as type to change the table structure */}
+    <TableMain menuCode="tableA"  />
+  </Provider>
+);
 
 export default App;
